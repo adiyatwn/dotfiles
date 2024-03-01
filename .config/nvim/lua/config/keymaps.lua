41c5c24a775da07adfccf7426d6f61a3a531c8d1
Adiyat Wira Negara <kazutonine@gmail.com>
2024-03-02 00:46:32 +0700

    Customize telescope, add telescope-file-browser

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -57,36 +57,12 @@ keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open diagnost
 
 -- Harpoon keymaps
 keymap.set("n", "<leader>a", function() harpoon:list():append() end)
---
--- basic telescope configuration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-  local file_paths = {}
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
-  end
-
-  require("telescope.pickers").new({}, {
-    prompt_title = "Harpoon",
-    finder = require("telescope.finders").new_table({
-      results = file_paths,
-    }),
-    previewer = conf.file_previewer({}),
-    sorter = conf.generic_sorter({}),
-  }):find()
-end
-
-vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-  { desc = "Open harpoon window" })
+keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 keymap.set("n", "<A-h>", function() harpoon:list():select(1) end)
 keymap.set("n", "<A-j>", function() harpoon:list():select(2) end)
 keymap.set("n", "<A-k>", function() harpoon:list():select(3) end)
 keymap.set("n", "<A-l>", function() harpoon:list():select(4) end)
-
--- Toggle previous & next buffers stored within Harpoon list
-keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 
 -- Open Notes folder
 keymap.set("n", "<leader>n", ":edit ~/notes<CR>", { desc = "Edit notes folder", noremap = true, silent = true })
