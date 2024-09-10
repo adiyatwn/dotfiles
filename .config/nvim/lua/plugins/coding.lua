91e23d047035b7894bddda17a11de18b42797f1e
Adiyat Wira Negara <kazutonine@gmail.com>
2024-09-10 12:10:08 +0700

    Change <CR> keymap to ignore the completetion, just use <C-y> instead

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,5 +1,26 @@
 return {
-  "smjonas/inc-rename.nvim",
-  cmd = "IncRename",
-  config = true,
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local auto_select = true
+
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+      }
+
+      opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
+        ["<CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+        end,
+        ["<C-y>"] = LazyVim.cmp.confirm({ select = auto_select }),
+      })
+    end,
+  },
 }
